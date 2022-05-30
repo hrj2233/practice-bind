da1829b689196868eb6c9e261f06972460c8cfff
hrj2233 <hrj2233@gmail.com>
2022-05-30 13:51:40 +0900

    practice bind

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 // Your code here
+const Employee = require("./employee");
+
+const employee1 = new Employee("John Wick", "Dog Lover");
+const employeeName = employee1.sayName.bind(employee1);
+const employeeOccupation = employee1.sayOccupation.bind(employee1);
+setTimeout(employeeName, 2000);
+setTimeout(employeeOccupation, 3000);
